eaf4f6af6eb1307b7b427d7377b822b79f9c7cb5
hunainhyder <mhh.awan2003@gmail.com>
2024-03-22 17:27:44 +0500

    added utility functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import dotenv from "dotenv"
-import express from "express"
 import db_conn from './db/index.js'
 
-const app = express()
-db_conn()
 dotenv.config()
+db_conn() 
+.then(() => {
+    app.on("error", () => {
+        console.error("ERROR: ", error)
+        throw error
+    })
 
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        
-//         app.on("error", () => {
-//             console.error("ERROR: ", error)
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.error("ERROR: ", error)
-//         throw err
-//     }
-// })()
+    app.listen(process.env.PORT, ()=> {
+        console.log(`Server is listening on ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log(`MongoDB connection failed ${err}`)
+})
